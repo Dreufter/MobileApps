019ba314818090271b3356df01bcf15e540b21e9
BuildTools <unconfigured@null.spigotmc.org>
2022-10-20 20:30:58 +0200

    #upload make onClick function

--- CardsGame/app/src/main/java/com/example/cardsgame/MainActivity.java
+++ CardsGame/app/src/main/java/com/example/cardsgame/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.cardsgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.imageButton1:
+
+                break;
+            case R.id.imageButton2:
+
+                break;
+            case R.id.imageButton3:
+
+                break;
+            case R.id.imageButton4:
+
+                break;
+            case R.id.imageButton5:
+
+                break;
+            case R.id.imageButton6:
+
+                break;
+            case R.id.imageButton7:
+
+                break;
+            case R.id.imageButton8:
+
+                break;
+            case R.id.imageButton9:
+
+                break;
+        }
     }
 }
