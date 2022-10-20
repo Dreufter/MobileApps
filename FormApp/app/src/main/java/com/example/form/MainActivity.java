6d45dbb1de8cf8461c758fbe23894429e8fbed50
BuildTools <unconfigured@null.spigotmc.org>
2022-10-20 18:01:29 +0200

    #upload first commit app structure created

---
+++ FormApp/app/src/main/java/com/example/form/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.form;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) throws NullPointerException {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Objects.requireNonNull(getSupportActionBar()).hide();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.mitem1:
+                Toast.makeText(this, "Elemento 1", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
