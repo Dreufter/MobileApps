d12bd1ccb487ba30b5cd9f8835f3ce3f100b9cc8
BuildTools <unconfigured@null.spigotmc.org>
2022-11-10 17:17:14 +0100

    #update Structure designed for activity_main.xml

--- RPSLS/app/src/main/java/com/example/myapplication/MainActivity.java
+++ RPSLS/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+
+                break;
+
+            case R.id.button2:
+
+                break;
+
+            case R.id.button3:
+
+                break;
+
+            case R.id.button4:
+
+                break;
+
+            case R.id.button5:
+
+                break;
+        }
     }
 }
